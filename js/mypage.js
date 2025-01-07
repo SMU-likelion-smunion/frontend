1c7edeb5abb4fc572400c477dc417129fccffb4b
yejin7457 <161455449+yejin7457@users.noreply.github.com>
2025-01-07 10:43:02 +0900

    fix : navbar 아이콘 active 오류 변경
    - navbar 아이콘 active 오류 수정
    - 탈퇴 모달 수정

--- js/mypage.js
+++ js/mypage.js
@@ -1,17 +1,43 @@
 // include.js
 window.addEventListener("load", function () {
-    var allElements = document.getElementsByTagName("*");
-    Array.prototype.forEach.call(allElements, function (el) {
-      var includePath = el.dataset.includePath;
-      if (includePath) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            el.outerHTML = this.responseText;
-          }
-        };
-        xhttp.open("GET", includePath, true);
-        xhttp.send();
-      }
-    });
+  var allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    var includePath = el.dataset.includePath;
+    if (includePath) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
   });
+});
+
+// 모달 관련 변수
+const deleteAccountModal = document.querySelector(".mypage-alert-container");
+const leaveClubRequest = document.querySelector(".leave-club-request");
+const deleteAccountRequest = document.querySelector(".delete-account");
+
+const cancelBtn = document.querySelector(".cancel");
+const deleteAccountdBtn = document.querySelector(".request");
+
+// 아이디 변경 모달 열기
+leaveClubRequest.addEventListener("click", () => {
+  deleteAccountModal.style.display = "flex";
+});
+
+deleteAccountRequest.addEventListener("click", () => {
+  deleteAccountModal.style.display = "flex";
+});
+
+// 모달 닫기
+cancelBtn.addEventListener("click", () => {
+  deleteAccountModal.style.display = "none";
+});
+
+deleteAccountdBtn.addEventListener("click", () => {
+  deleteAccountModal.style.display = "none";
+});
